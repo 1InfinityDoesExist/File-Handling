6d4aa93f4620fcae7bf4b5d04416219d3b30680a
Avinash Patel <avinash.patel@gaiansolutions.com>
2020-11-30 11:55:40 +0530

    api implemented

--- src/main/java/in/csv/csvManipulate/serviceImpl/CountryDetailsServiceImpl.java
+++ src/main/java/in/csv/csvManipulate/serviceImpl/CountryDetailsServiceImpl.java
@@ -1,8 +1,25 @@
 package in.csv.csvManipulate.serviceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import in.csv.csvManipulate.kafka.producer.CountryDetailsProducer;
+import in.csv.csvManipulate.repository.CountryDetailsRepository;
+import in.csv.csvManipulate.request.CountryDetailsCreateRequest;
+import in.csv.csvManipulate.service.CountryDetailsService;
+
 @Service
-public class CountryDetailsServiceImpl {
+public class CountryDetailsServiceImpl implements CountryDetailsService {
+
+	@Autowired
+	private CountryDetailsProducer countryDetailsproducer;
+
+	@Autowired
+	private CountryDetailsRepository countryDetailsRepository;
+
+	@Override
+	public void saveCountryDetails(CountryDetailsCreateRequest request) {
+		countryDetailsproducer.produce("countryDetails", request.toString());
+	}
 
 }
