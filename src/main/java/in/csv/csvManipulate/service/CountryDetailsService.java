3c12806263648a617100c057e5b72e71b6776ca8
Avinash Patel <avinash.patel@gaiansolutions.com>
2020-12-01 08:39:31 +0530

    refactor

--- src/main/java/in/csv/csvManipulate/service/CountryDetailsService.java
+++ src/main/java/in/csv/csvManipulate/service/CountryDetailsService.java
@@ -1,12 +1,19 @@
 package in.csv.csvManipulate.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
+import in.csv.csvManipulate.entity.CountryDetails;
 import in.csv.csvManipulate.request.CountryDetailsCreateRequest;
 
 @Component
 public interface CountryDetailsService {
 
 	public void saveCountryDetails(CountryDetailsCreateRequest request);
+
+	public List<CountryDetails> getAllCountryDetails();
+
+	public List<CountryDetails> getAllCountryBasedOnBoundary(List<String> boarders);
 
 }
--- src/main/java/in/csv/csvManipulate/serviceImpl/CountryDetailsServiceImpl.java
+++ src/main/java/in/csv/csvManipulate/serviceImpl/CountryDetailsServiceImpl.java
@@ -1,13 +1,18 @@
 package in.csv.csvManipulate.serviceImpl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import in.csv.csvManipulate.entity.CountryDetails;
 import in.csv.csvManipulate.kafka.producer.CountryDetailsProducer;
-import in.csv.csvManipulate.repository.CountryDetailsRepository;
 import in.csv.csvManipulate.request.CountryDetailsCreateRequest;
 import in.csv.csvManipulate.service.CountryDetailsService;
 import lombok.extern.slf4j.Slf4j;
@@ -20,7 +25,7 @@ public class CountryDetailsServiceImpl implements CountryDetailsService {
 	private CountryDetailsProducer countryDetailsproducer;
 
 	@Autowired
-	private CountryDetailsRepository countryDetailsRepository;
+	private MongoTemplate mongoTemplate;
 
 	@Override
 	public void saveCountryDetails(CountryDetailsCreateRequest request) {
@@ -29,9 +34,21 @@ public class CountryDetailsServiceImpl implements CountryDetailsService {
 			log.info("::::request {}", request);
 			countryDetailsproducer.produce("countryDetails", new ObjectMapper().writeValueAsString(request));
 		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public List<CountryDetails> getAllCountryDetails() {
+		return mongoTemplate.findAll(CountryDetails.class);
+	}
+
+	@Override
+	public List<CountryDetails> getAllCountryBasedOnBoundary(List<String> boarders) {
+		Query query = new Query();
+		Criteria criteria = Criteria.where("borders").all(boarders);
+		query.addCriteria(criteria);
+		return mongoTemplate.find(query, CountryDetails.class);
 	}
 
 }
